9cb04e120dd64a4c64cf723ccc8059c2da2e1bd4
rgrohitgupta938 <49370602+rgrohitgupta938@users.noreply.github.com>
2024-04-12 01:36:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-// your code here
+ const btn = document.getElementById("button");
+    btn.addEventListener("click", () => {
+      const h = document.getElementById("url");
+      const name = document.getElementById("name").value;
+      const year = document.getElementById("year").value;
+      let url = "https://localhost:8080/";
+
+      if (name) {
+        url += `?name=${name}`;
+      }
+      if (year) {
+        url += `&year=${year}`;
+      }
+      h.textContent = url;
+    });
