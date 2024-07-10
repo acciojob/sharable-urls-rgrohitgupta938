87b94649b5372536f2d13104c507a2d081a0b5b9
rgrohitgupta938 <49370602+rgrohitgupta938@users.noreply.github.com>
2024-07-10 17:43:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
- const btn = document.getElementById("button");
-    btn.addEventListener("click", (e) => {
-		e.preventDefault();
-      const h = document.getElementById("url");
-      const name = document.getElementById("name").value;
-      const year = document.getElementById("year").value;
-      let url = "https://localhost:8080/";
+const btn = document.getElementById("button");
+const text = document.getElementById("url");
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  const name = document.getElementById("name").value;
+  const year = document.getElementById("year").value;
+  let url = "https://localhost:8080/";
+  console.log(name, year);
 
-      if (name) {
-        url += `?name=${name}`;
-      }
-      if (year) {
-        url += `&year=${year}`;
-      }
-      h.textContent = url;
-    });
+  if (name) {
+    url += `?name=${name}`;
+  }
+  if (year) {
+    url += name ? `&year=${year}` : `?year=${year}`;
+  }
+  text.textContent = url;
+});
